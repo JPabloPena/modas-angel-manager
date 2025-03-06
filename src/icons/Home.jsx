8826e9ee44adfd8a47d6fe5501c5671f9e6f59b1
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-05 21:32:08 -0500

    chore: remove unnecesary React import

--- src/icons/Home.jsx
+++ src/icons/Home.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Home ({ className = 'size-6' }) {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className={className}>
--- src/icons/LogOut.jsx
+++ src/icons/LogOut.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function LogOut ({ className = 'size-6' }) {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className={className}>
