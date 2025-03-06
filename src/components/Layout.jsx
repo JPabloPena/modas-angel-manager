88caa4eb977f3312e9fb91a8addbd0fe926325fb
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-05 21:28:57 -0500

    feat: add Topbar and update styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import Sidebar from './Sidebar'
+import Topbar from './Topbar'
 
 function Layout ({ children }) {
   return (
-    <>
+    <div className='flex w-full h-dvh p-2 bg-primary-bg'>
       <Sidebar />
-    </>
+      <div className='w-full '>
+        <Topbar />
+        {children}
+      </div>
+    </div>
   )
 }
 
